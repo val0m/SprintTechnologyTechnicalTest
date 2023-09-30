3e0fbb7ddc662f530184f9790cc2c84d67ef84db
Valentin PASSE <valentin.passe.ext@tidyuptech.com>
2023-09-30 10:13:57 +0200

    [Api] Change name project

--- SolBoardingCardsTechnicalTest/BoardingCardsTechnicalTest/Controllers/WeatherForecastController.cs
+++ SolBoardingCardsTechnicalTest/BoardingCardsTechnicalTest/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace BoardingCardsTechnicalTest.Controllers
+namespace BoardingCards.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- SolBoardingCardsTechnicalTest/BoardingCardsTechnicalTest/WeatherForecast.cs
+++ SolBoardingCardsTechnicalTest/BoardingCardsTechnicalTest/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace BoardingCardsTechnicalTest
+namespace BoardingCards
 {
     public class WeatherForecast
     {
