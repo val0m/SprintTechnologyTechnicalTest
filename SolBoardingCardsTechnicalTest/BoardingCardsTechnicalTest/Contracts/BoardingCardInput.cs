55de1357c26b9b993ff183c7048511a8bd3ea602
Valentin PASSE <valentin.passe.ext@tidyuptech.com>
2023-09-30 11:40:42 +0200

    [Api] Add input for api

---
+++ SolBoardingCardsTechnicalTest/BoardingCardsTechnicalTest/Contracts/BoardingCardInput.cs
@@ -0,0 +1,15 @@
+﻿using BoardingCards.DomainShared.Enums;
+
+namespace BoardingCards.Contracts
+{
+    public class BoardingCardInput
+    {
+        public TransportType Type { get; init; }
+        public required string Departure { get; init; }
+        public required string Destination { get; init; }
+        public string? SeatNumber { get; set; }
+        public string? TransportNumber { get; set; }
+        public string? GateNumber { get; set; }
+        public string? BaggageTicketCounter { get; set; }
+    }
+}
